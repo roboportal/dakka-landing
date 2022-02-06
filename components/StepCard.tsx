1b3fa81d2c85a49469f3b0f74c89680d136dd321
Denys Malykhin <denys.malykhin@solidopinion.com>
2022-02-06 14:30:42 -0800

    quick fix

--- components/StepCard.tsx
+++ components/StepCard.tsx
@@ -45,6 +45,7 @@ export default function StepCard({
             justify-content: center;
             align-items: center;
             font-weight: 600;
+            color: black;
             box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 1px -1px,
               rgba(0, 0, 0, 0.14) 0px 1px 1px 0px,
               rgba(0, 0, 0, 0.12) 0px 1px 3px 0px;
